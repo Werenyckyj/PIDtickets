8d0c43de0c1df4fed99765f8b95594623eb234ad
Werenyckyj <werenyckyjvojtech@gmail.com>
2022-10-31 21:50:07 +0100

    SocketException(10061)

--- PIDtickets/PIDtickets/MainPage.xaml.cs
+++ PIDtickets/PIDtickets/MainPage.xaml.cs
@@ -63,8 +63,11 @@ namespace PIDtickets
         {
             HttpContent content = new StringContent(s, Encoding.UTF8, "application/json");
             HttpClient htc = new HttpClient();
-            string address = "https://nominatim.openstreetmap.org/?addressdetails=" + s + "&format=json&limit=1";
-            HttpResponseMessage response = await htc.PostAsync(address, content);
+            string address = "https://nominatim.openstreetmap.org/search?q=" + s + "&format=geojson"; //Proč yhazuje chybu? Chyba: Server neslyší:System.Net.Http.HttpRequestException: Nemohlo být vytvořeno žádné připojení, protože cílový počítač je aktivně odmítl.
+                                                                                                      //--->System.Net.Sockets.SocketException(10061): Nemohlo být vytvořeno žádné připojení, protože cílový počítač je aktivně odmítl.
+                                                                                                      //at System.Net.Http.ConnectHelper.ConnectAsync(String host, Int32 port, CancellationToken cancellationToken)
+                                                                                                      //-- - End of inner exception stack trace-- -
+             HttpResponseMessage response = await htc.PostAsync(address, content);
             Out.Text = response.ToString() + Environment.NewLine + response.IsSuccessStatusCode.ToString() + Environment.NewLine + response.StatusCode + Environment.NewLine + response.Content.ToString();
         }
     }
